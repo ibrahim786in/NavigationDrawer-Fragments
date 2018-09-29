bb96bb467e68b4b18013a2e00bee79d88ebf4454
ibrahim786in <md.ibrahim786in@gmail.com>
2018-09-29 18:43:53 +0530

    Button color changed when click from navigation View.

--- app/src/main/java/com/example/ibrahim/fragmentspracapp/MainActivity.kt
+++ app/src/main/java/com/example/ibrahim/fragmentspracapp/MainActivity.kt
@@ -12,36 +12,52 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val fManager = supportFragmentManager
-
+    val color = Color.parseColor("#000000")
+fun colorCode(){
+    home_btn.setTextColor(Color.parseColor("#ff669900"))
+    movies_btn.setTextColor(Color.parseColor("#ffff8800"))
+    games_btn.setTextColor(Color.parseColor("#ffaa66cc"))
+    extras_btn.setTextColor(Color.parseColor("#ff33b5e5"))
+}
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-       val color=Color.parseColor("#000000")
+
         when (item.itemId) {
             R.id.nav_home -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, HomeFragment()).commit()
-                home_btn.isSelected
+                home_btn.isSelected=true
+
+                colorCode()
                 home_btn.setTextColor(color)
+
+
             }
             R.id.nav_movies -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, MoviesFragment()).commit()
-                movies_btn.isSelected
-//                movies_btn.setTextColor(color)
+                movies_btn.isSelected=true
+                colorCode()
+
+                movies_btn.setTextColor(color)
+
             }
             R.id.nav_games -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, GamesFragment()).commit()
-                games_btn.isSelected
-//                games_btn.setTextColor(color)
+                games_btn.isSelected=true
+                colorCode()
+                games_btn.setTextColor(color)
             }
             R.id.nav_extras -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, ExtrasFragment()).commit()
-                extras_btn.isSelected
-//                extras_btn.setTextColor(color)
+                extras_btn.isSelected=true
+                colorCode()
+                extras_btn.setTextColor(color)
+
             }
+
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,23 +67,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
         nav_view.setNavigationItemSelectedListener(this)
         Log.i("FragmentMainActivity", "onCreate")
-        fManager.beginTransaction().add(R.id.mainActFragment, HomeFragment()).commit()
+        fManager.beginTransaction().add(R.id.mainActFragment, HomeFragment()).addToBackStack(null).commit()
 
+        home_btn.isPressed=true
 
         home_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, HomeFragment()).commit()
         }
         games_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, GamesFragment()).commit()
+            home_btn.isPressed=false
         }
         extras_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, ExtrasFragment()).commit()
+            home_btn.isPressed=false
         }
         movies_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, MoviesFragment()).commit()
+            home_btn.isPressed=false
         }
     }
 }
