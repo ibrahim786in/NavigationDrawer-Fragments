edc3185239f4adeb29311cbf33e789ac2f61416c
ibrahim786in <md.ibrahim786in@gmail.com>
2018-10-01 19:40:53 +0530

    Some Modifications on navigation menu

--- app/src/main/java/com/example/ibrahim/fragmentspracapp/MainActivity.kt
+++ app/src/main/java/com/example/ibrahim/fragmentspracapp/MainActivity.kt
@@ -13,46 +13,57 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val fManager = supportFragmentManager
     val color = Color.parseColor("#000000")
-fun colorCode(){
-    home_btn.setTextColor(Color.parseColor("#ff669900"))
-    movies_btn.setTextColor(Color.parseColor("#ffff8800"))
-    games_btn.setTextColor(Color.parseColor("#ffaa66cc"))
-    extras_btn.setTextColor(Color.parseColor("#ff33b5e5"))
-}
+    private fun colorCode() {
+        home_btn.setTextColor(Color.parseColor("#ff669900"))
+        movies_btn.setTextColor(Color.parseColor("#ffff8800"))
+        games_btn.setTextColor(Color.parseColor("#ffaa66cc"))
+        extras_btn.setTextColor(Color.parseColor("#ff33b5e5"))
+    }
+
+    private fun notPressed() {
+        home_btn.isPressed = false
+        movies_btn.isPressed = false
+        games_btn.isPressed = false
+        extras_btn.isPressed = false
+        images_btn.isPressed = false
+    }
+
+    //    lateinit var commonItem: MenuItem
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        notPressed()
+        item.isChecked = true
 
         when (item.itemId) {
             R.id.nav_home -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, HomeFragment()).commit()
-                home_btn.isSelected=true
+                notPressed()
+                home_btn.isPressed = true
 
                 colorCode()
                 home_btn.setTextColor(color)
-
-
             }
             R.id.nav_movies -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, MoviesFragment()).commit()
-                movies_btn.isSelected=true
+                notPressed()
+                movies_btn.isPressed = true
                 colorCode()
 
                 movies_btn.setTextColor(color)
-
             }
             R.id.nav_games -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, GamesFragment()).commit()
-                games_btn.isSelected=true
+                notPressed()
+                games_btn.isPressed = true
                 colorCode()
                 games_btn.setTextColor(color)
             }
             R.id.nav_extras -> {
                 fManager.beginTransaction().replace(R.id.mainActFragment, ExtrasFragment()).commit()
-                extras_btn.isSelected=true
+                notPressed()
+                extras_btn.isPressed = true
                 colorCode()
                 extras_btn.setTextColor(color)
-
             }
-
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
@@ -63,6 +74,8 @@ fun colorCode(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        notPressed()
+
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
@@ -70,23 +83,36 @@ fun colorCode(){
         nav_view.setNavigationItemSelectedListener(this)
         Log.i("FragmentMainActivity", "onCreate")
         fManager.beginTransaction().add(R.id.mainActFragment, HomeFragment()).addToBackStack(null).commit()
-
-        home_btn.isPressed=true
+        onNavigationItemSelected(nav_view.menu.getItem(0))
 
         home_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, HomeFragment()).commit()
-        }
-        games_btn.setOnClickListener {
-            fManager.beginTransaction().replace(R.id.mainActFragment, GamesFragment()).commit()
-            home_btn.isPressed=false
-        }
-        extras_btn.setOnClickListener {
-            fManager.beginTransaction().replace(R.id.mainActFragment, ExtrasFragment()).commit()
-            home_btn.isPressed=false
+            notPressed()
+            onNavigationItemSelected(nav_view.menu.getItem(0))
+            home_btn.isPressed = true
+
+//        onNavigationItemSelected(commonItem)
         }
         movies_btn.setOnClickListener {
             fManager.beginTransaction().replace(R.id.mainActFragment, MoviesFragment()).commit()
-            home_btn.isPressed=false
+            notPressed()
+            onNavigationItemSelected(nav_view.menu.getItem(1))
+            movies_btn.isPressed = true
+
+        }
+        games_btn.setOnClickListener {
+            fManager.beginTransaction().replace(R.id.mainActFragment, GamesFragment()).commit()
+            notPressed()
+            onNavigationItemSelected(nav_view.menu.getItem(2))
+            games_btn.isPressed = true
+
+        }
+        extras_btn.setOnClickListener {
+            fManager.beginTransaction().replace(R.id.mainActFragment, ExtrasFragment()).commit()
+            notPressed()
+            onNavigationItemSelected(nav_view.menu.getItem(3))
+            extras_btn.isPressed = true
+
         }
     }
 }
